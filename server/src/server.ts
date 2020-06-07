7698433887c9c83bfcc1184812da613ddbeeae7e
Valney Junior <neymarinho.junior@gmail.com>
2020-06-07 19:02:27 -0400

    :art: fix get image

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'img')));
 
 app.use(errors());
 
